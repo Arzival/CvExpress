710b2ca4785aa661e119d4f56608527ad985901e
Arzival <arzival.sistemas@gmail.com>
2022-01-12 16:01:50 -0600

    guardado de los datos en DB

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -35,9 +35,63 @@ class CVController extends Controller
      */
     public function store(Request $request)
     {
-        // $foto['file'] = time() . '_' . $request->file('file')->getClientOriginalName();
-        // $request->file('file')->storeAs('public/foto', $foto['file']);
-        return $request;
+        $foto['file'] = time() . '_' . $request->file('file')->getClientOriginalName();
+        $request->file('file')->storeAs('public/foto', $foto['file']);
+        CV::create([
+            'estilo' => $request['estilo'],
+            'file' => $foto['file'],
+            'nombre' => $request['nombre'],
+            'apellidos' => $request['apellidos'],
+            'edad' => $request['edad'],
+            'telefono1' => $request['telefono1'],
+            'telefono2' => $request['telefono2'],
+            'email' => $request['email'],
+            'nivel_estudios' => $request['nivel_estudios'],
+            'nombre_institucion' => $request['nombre_institucion'],
+            'fecha_egreso' => $request['fecha_egreso'],
+            'aptitud1' => $request['aptitud1'],
+            'aptitud2' => $request['aptitud2'],
+            'aptitud3' => $request['aptitud3'],
+            'aptitud4' => $request['aptitud4'],
+            'aptitud5' => $request['aptitud5'],
+            'conocimiento1' => $request['conocimiento1'],
+            'conocimiento2' => $request['conocimiento2'],
+            'conocimiento3' => $request['conocimiento3'],
+            'conocimiento4' => $request['conocimiento4'],
+            'conocimiento5' => $request['conocimiento5'],
+            'idioma1' => $request['idioma1'],
+            'idioma2' => $request['idioma2'],
+            'idioma3' => $request['idioma3'],
+            'idioma4' => $request['idioma4'],
+            'idioma5' => $request['idioma5'],
+            'empresa1' => $request['empresa1'],
+            'puesto1' => $request['puesto1'],
+            'fecha_ingreso1' => $request['fecha_ingreso1'],
+            'fecha_egreso1' => $request['fecha_egreso1'],
+            'descripcion_puesto1' => $request['descripcion_puesto1'],
+            'empresa2' => $request['empresa2'],
+            'puesto2' => $request['puesto2'],
+            'fecha_ingreso2' => $request['fecha_ingreso2'],
+            'fecha_egreso2' => $request['fecha_egreso2'],
+            'descripcion_puesto2' => $request['descripcion_puesto2'],
+            'empresa3' => $request['empresa3'],
+            'puesto3' => $request['puesto3'],
+            'fecha_ingreso3' => $request['fecha_ingreso3'],
+            'fecha_egreso3' => $request['fecha_egreso3'],
+            'descripcion_puesto3' => $request['descripcion_puesto3'],
+            'empresa4' => $request['empresa4'],
+            'puesto4' => $request['puesto4'],
+            'fecha_ingreso4' => $request['fecha_ingreso4'],
+            'fecha_egreso4' => $request['fecha_egreso4'],
+            'descripcion_puesto4' => $request['descripcion_puesto4'],
+            'empresa5' => $request['empresa5'],
+            'puesto5' => $request['puesto5'],
+            'fecha_ingreso5' => $request['fecha_ingreso5'],
+            'fecha_egreso5' => $request['fecha_egreso5'],
+            'descripcion_puesto5' => $request['descripcion_puesto5'],
+        ]);
+        
+        return redirect()->route('home');
     }
 
     /**
